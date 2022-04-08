30447aea0afe1de3be734fa8876fc983f1ef6d0c
Galahad2819 <akhilnagar19@gmail.com>
2022-04-09 00:42:35 +0530

    Added dropdown functionality

--- app/src/main/java/com/example/boredapp/display1.java
+++ app/src/main/java/com/example/boredapp/display1.java
@@ -10,7 +10,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,34 +39,63 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 
 public class display1 extends AppCompatActivity {
-    EditText type;
+    String type="education";
     EditText participants;
     String activity;
     TextView text1;
-
-
+    Spinner dropdown;
 
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
-        type = (EditText) findViewById(R.id.type);
+
         participants = (EditText) findViewById(R.id.participants);
         text1 = (TextView) findViewById(R.id.text1);
-        Log.i("Hi","Hi1");
+        dropdown = findViewById(R.id.spinner2);
+        String[] items = new String[]{"education", "recreation", "social", "charity", "cooking","relaxation","busywork","diy"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
+
     public void todo(View view) {
         setContentView(R.layout.display);
-        type = (EditText) findViewById(R.id.type);
         text1 = (TextView) findViewById(R.id.text1);
         participants = (EditText) findViewById(R.id.participants);
+        dropdown = findViewById(R.id.spinner2);
+        String[] items = new String[]{"education", "recreation", "social", "charity", "cooking","relaxation","busywork","diy"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                //Toast.makeText(display1.this, dropdown.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+                //type = dropdown.getSelectedItem().toString();
+                type = parent.getItemAtPosition(position).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        Log.i("TAG","aaaa"+type);
+        Log.i("TAG","bbbb"+participants.getText());
         String postUrl= "https://still-temple-97961.herokuapp.com/hello-servlet";
         String postBody="{\n" +
-                "    \"type\": \""+type.getText()+"\",\n" +
+                "    \"type\": \""+type+"\",\n" +
                 "    \"participants\": \""+participants.getText()+"\"\n" +
                 "}";
+        Log.i("TAG","111111"+postBody);
         try {
             postRequest(postUrl,postBody);
         } catch (IOException e) {
@@ -86,6 +118,7 @@ public class display1 extends AppCompatActivity {
             @Override
             public void onFailure(Call call, IOException e) {
                 call.cancel();
+                text1.setText("No activity found!");
             }
 
             @Override
